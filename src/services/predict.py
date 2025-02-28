6decef9397bb4c2a673ab2197379e5393bc7c600
nextpassword99 <inderscop@gmail.com>
2025-02-28 18:48:58 -0500

    feat: Añadir servicio de predicción para clasificación de imágenes usando un modelo cargado

---
+++ src/services/predict.py
@@ -0,0 +1,43 @@
+from tensorflow.keras.models import load_model
+import cv2
+import numpy as np
+
+
+class PredictService:
+    def __init__(self, path_model):
+        self.model = load_model(path_model)
+
+    def start(self, image_path):
+        if self.model is None:
+            print('No se ha cargado el modelo')
+            return None
+        if image_path is None:
+            print('Sube una imagen')
+            return None
+
+        process_image = self._preprocess_image(image_path)
+        if process_image is None:
+            print('Error al procesar la imagen')
+            return None
+
+        prediction = self._predict_image(process_image)
+        return prediction
+
+    def _preprocess_image(self, image_path, target_size=(150, 150)):
+        try:
+            img = cv2.imread(image_path)
+            if img is None:
+                print(f'No se pudo cargar la imagen: {image_path}')
+                return None
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img_resized = cv2.resize(img, target_size)
+            img_normalized = img_resized / 255.0
+
+            return img_normalized.astype(np.float32)
+        except Exception as e:
+            print(f'Error al procesar la imagen: {e}')
+            return None
+
+    def _predict_image(self, img):
+        img = np.expand_dims(img, axis=0)
+        return self.model.predict(img)[0][0]
