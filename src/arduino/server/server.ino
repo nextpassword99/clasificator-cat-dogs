8fc56ddd57c6ef5ab1d1ab6717fd2db901a7327e
nextpassword99 <inderscop@gmail.com>
2025-02-28 18:48:38 -0500

    feat: Añadir servidor web para controlar LEDs de clasificación de gatos y perros en ESP8266

---
+++ src/arduino/server/server.ino
@@ -0,0 +1,82 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+const char *ssid = "System32";
+const char *pass = "edisonp21";
+
+ESP8266WebServer server(80);
+
+const int ledDog = 2;
+const int ledCat = 4;
+
+void setup()
+{
+    Serial.begin(115200);
+    WiFi.begin(ssid, pass);
+
+    pinMode(ledDog, OUTPUT);
+    pinMode(ledCat, OUTPUT);
+
+    digitalWrite(ledDog, LOW);
+    digitalWrite(ledCat, LOW);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(1000);
+        Serial.println("Conectando...");
+    }
+
+    Serial.println(WiFi.localIP());
+
+    server.on("/led", HTTP_GET, handleRequest);
+
+    server.begin();
+}
+
+void loop()
+{
+    server.handleClient();
+}
+
+void handleRequest()
+{
+    if (!server.hasArg("predict"))
+    {
+        server.send(400, "text/plain", "Falta el parámetro 'predict'");
+        return;
+    }
+
+    String predict = server.arg("predict");
+    float value = predict.toFloat();
+
+    if (value > 0.5)
+    {
+        isDog();
+    }
+    else
+    {
+        isCat();
+    }
+
+    server.send(200, "text/plain", "LED actualizado: " + value > 0.5 ? "Perro" : "Gato");
+}
+
+void isDog()
+{
+    digitalWrite(ledCat, LOW);
+    digitalWrite(ledDog, HIGH);
+}
+
+void isCat()
+{
+    digitalWrite(ledDog, LOW);
+    digitalWrite(ledCat, HIGH);
+}
+
+void none()
+{
+    delay(10000);
+    Serial.println("Apagando LEDs");
+    digitalWrite(ledDog, LOW);
+    digitalWrite(ledCat, LOW);
+}
