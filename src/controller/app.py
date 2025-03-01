c511334374f01986ae147e8335e89fcfc0898a39
nextpassword99 <inderscop@gmail.com>
2025-02-28 19:39:45 -0500

    feat: Añadir controlador para predicción de imágenes y comunicación con ESP8266

---
+++ src/controller/app.py
@@ -0,0 +1,20 @@
+import requests
+from src.services.predict import PredictService
+
+
+path_model = 'models/cat_dog_classifier_improved5.h5'
+predict_service = PredictService(path_model)
+
+
+while True:
+    image_path = input('Ruta de la imagen: ')
+
+    prediction = predict_service.start(image_path)
+    print(prediction)
+
+    path_esp = 'http://192.168.18.250/led'
+
+    params = {'predict': prediction}
+    response = requests.get(path_esp, params=params)
+
+    print(response.text)
